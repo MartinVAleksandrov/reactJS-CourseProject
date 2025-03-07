86cf60ddd2437993b81e4b8ef29d8fa32137183a
MartinVAleksandrov <veenn4@gmail.com>
2025-03-07 20:53:25 +0200

    add module css for login form

--- reactJSProject/src/assets/LoginForm/LoginForm.jsx
+++ reactJSProject/src/assets/LoginForm/LoginForm.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import "./LoginForm.css";
+import styles from "../LoginForm/LoginForm.module.css";
 
 const LoginForm = () => {
   return (
-    <div className="wrapper">
+    <div className={styles.wrapper}>
       <form action="">
         <h1>Login</h1>
-        <div className="input-box">
+        <div className={styles.inputbox}>
           <input type="text" placeholder="Username" required />
         </div>
-        <div className="input-box">
+        <div className={styles.inputbox}>
           <input type="password" placeholder="Password" required />
         </div>
         <button type="submit">Login</button>
-        <div className="register-link">
+        <div className={styles.registerlink}>
           <p>
             Don't have an account? <a href="#">Register</a>
           </p>
