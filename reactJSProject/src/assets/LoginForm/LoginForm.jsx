18aea284e3e16e6e6a48c4d580f58c9388130de7
MartinVAleksandrov <veenn4@gmail.com>
2025-03-10 20:25:31 +0200

    Add simple validation for login and register

--- reactJSProject/src/assets/LoginForm/LoginForm.jsx
+++ reactJSProject/src/assets/LoginForm/LoginForm.jsx
@@ -1,19 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "../LoginForm/LoginForm.module.css";
 import { Link } from "react-router-dom";
 
 const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
+  const navigate = useNavigate();
+
+  const validateForm = () => {
+    let errors = {};
+
+    if (!email.trim()) {
+      errors.email = "Email is required";
+    } else if (!/^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/.test(email)) {
+      errors.email = "Invalid email format";
+    }
+
+    if (!password.trim()) {
+      errors.password = "Password is required";
+    } else if (password.length < 6) {
+      errors.password = "Password must be at least 6 characters";
+    }
+
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      console.log("Form submitted", { email, password });
+      navigate("/");
+    }
+  };
+
   return (
     <div className={styles.wrapper}>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <h1>Login</h1>
+
         <div className={styles.inputbox}>
-          <input type="text" placeholder="Username" required />
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="text"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label className={styles.errorLabel}>{errors.email || ""}</label>
         </div>
+        <br />
+
         <div className={styles.inputbox}>
-          <input type="password" placeholder="Password" required />
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label className={styles.errorLabel}>{errors.password || ""}</label>
         </div>
+        <br />
+        <br />
         <button type="submit">Login</button>
+
         <div className={styles.registerlink}>
           <p>
             Don't have an account? <Link to="/RegisterForm">Register</Link>
@@ -23,4 +78,5 @@ const LoginForm = () => {
     </div>
   );
 };
+
 export default LoginForm;
--- reactJSProject/src/assets/RegisterForm/RegisterForm.jsx
+++ reactJSProject/src/assets/RegisterForm/RegisterForm.jsx
@@ -1,29 +1,140 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "../RegisterForm/RegisterForm.module.css";
-import { Link } from "react-router-dom";
 
 const RegisterForm = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errors, setErrors] = useState({});
+  const navigate = useNavigate();
+
+  const validateForm = () => {
+    let errors = {};
+
+    if (!firstName.trim()) {
+      errors.firstName = "First name is required";
+    }
+
+    if (!lastName.trim()) {
+      errors.lastName = "Last name is required";
+    }
+
+    if (!email.trim()) {
+      errors.email = "Email is required";
+    } else if (!/^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/.test(email)) {
+      errors.email = "Invalid email format";
+    }
+
+    if (!password.trim()) {
+      errors.password = "Password is required";
+    } else if (password.length < 6) {
+      errors.password = "Password must be at least 6 characters";
+    }
+
+    if (!confirmPassword.trim()) {
+      errors.confirmPassword = "Confirm Password is required";
+    } else if (password !== confirmPassword) {
+      errors.confirmPassword = "Passwords do not match";
+    }
+
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      console.log("Registration successful", {
+        firstName,
+        lastName,
+        email,
+        password,
+      });
+
+      navigate("/");
+    }
+  };
+
   return (
     <div className={styles.wrapper}>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <h1>Register</h1>
+
         <div className={styles.inputbox}>
-          <input type="text" placeholder="First name" required />
+          <label htmlFor="firstName">First Name</label>
+          <input
+            id="firstName"
+            type="text"
+            placeholder="Enter your first name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <label className={styles.errorLabel}>{errors.firstName || ""}</label>
         </div>
+        <br />
+
         <div className={styles.inputbox}>
-          <input type="text" placeholder="Last name" required />
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            id="lastName"
+            type="text"
+            placeholder="Enter your last name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <label className={styles.errorLabel}>{errors.lastName || ""}</label>
         </div>
+        <br />
+
         <div className={styles.inputbox}>
-          <input type="password" placeholder="Password" required />
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="text"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label className={styles.errorLabel}>{errors.email || ""}</label>
         </div>
+        <br />
+
         <div className={styles.inputbox}>
-          <input type="password" placeholder="Confirm Password" required />
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label className={styles.errorLabel}>{errors.password || ""}</label>
         </div>
-        <Link to="/">
-          <button type="submit">Register</button>
-        </Link>
+        <br />
+
+        <div className={styles.inputbox}>
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            id="confirmPassword"
+            type="password"
+            placeholder="Confirm your password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <label className={styles.errorLabel}>
+            {errors.confirmPassword || ""}
+          </label>
+        </div>
+        <br />
+        <br />
+
+        <button type="submit">Register</button>
       </form>
     </div>
   );
 };
+
 export default RegisterForm;
